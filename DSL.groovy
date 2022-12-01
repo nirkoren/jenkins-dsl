2d602dd13871d1facd1eee9b7d96223a828e2ea0
Nir Koren <hanirnir@gmail.com>
2022-12-01 10:31:51 +0200

    Create DSL.groovy

---
+++ DSL.groovy
@@ -0,0 +1,29 @@
+import groovy.json.JsonSlurper
+
+def reposJSON = new JsonSlurper().parseText(readFileFromWorkspace('repos.json'))
+
+reposJSON.repos.each {
+    createPipeline(it)
+}
+
+void createPipeline(it) {
+
+    def jobname = it.jobname
+    def gitrepo = it.gitrepo
+    def desc = it.description
+    pipelineJob(jobname) {
+        description(desc)
+
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(gitrepo)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
